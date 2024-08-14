63765667c3e3ed56618e130a2531c99458613f60
sorey <thefool@mail.ustc.edu.cn>
2024-08-14 11:08:39 +0800

    fix db

--- lib/config/db.ts
+++ lib/config/db.ts
@@ -4,6 +4,6 @@ import mongoose from "mongoose"
  * 连接到 mongodb atlas 数据库
  */
 export const ConnectDB = async () => {
-  await mongoose.connect('mongodb+srv://admin:Mongo122128@cluster0.cdmv9.mongodb.net/todo-app')
+  await mongoose.connect('mongodb+srv://<username>:<password>@cluster0.cdmv9.mongodb.net/<project-name>')
   console.log("db connected...")
 }
